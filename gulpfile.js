38763e5a1d3c2ffc8822d3a4b9f2e689d59a6724
gulnazkr <krgulnoza@mail.ru>
2021-10-05 20:37:54 +0200

    start of project

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync');
+const { task } = require('gulp');
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({ stream: true }))
+
+});
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
+});
+gulp.task('js', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+gulp.task('watch', function() {
+    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
+});
